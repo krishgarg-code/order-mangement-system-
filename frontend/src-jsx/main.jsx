9ba97a25a1f646a90ac122db72359bf9e7d3759e
GreenHacker <hhirawat5@gmail.com>
2025-06-12 17:28:54 +0530

    feat: Add favicon files and update index.html to include them

--- frontend/src-jsx/main.jsx
+++ frontend/src-jsx/main.jsx
@@ -1,6 +1,5 @@
-import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(/*#__PURE__*/ _jsxDEV(App, {}));
+createRoot(document.getElementById("root")).render(<App />);
